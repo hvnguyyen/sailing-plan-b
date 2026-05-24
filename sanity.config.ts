e80721be7eb80fd89427a5c8565a32c2bd0f63c0
hvnguyyen <nguyen.hoangvinh@outlook.com>
2026-05-24 16:45:56 +0200

    fix TypeScript error: annotate templates prev parameter as Template[]

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,7 @@
 'use client'
 
 import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
+import {defineConfig, type Template} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {media} from 'sanity-plugin-media'
 
@@ -22,7 +22,7 @@ export default defineConfig({
     heicPlugin(),
   ],
   // When creating a child album from within a parent folder, auto-assign the parent
-  templates: (prev) => [
+  templates: (prev: Template[]) => [
     ...prev,
     {
       id: 'album-child',
